e10fcb46dc3d8ad4998ab31d9a24e338be1b4e4f
OwethuSotomela <owethusotomela@gmail.com>
2022-02-03 23:49:47 +0200

    Using named routes

--- lib/category_meals_screen.dart
+++ lib/category_meals_screen.dart
@@ -1,16 +1,21 @@
 import 'package:flutter/material.dart';
+import './main.dart';
 
 class CategoryMealsScreens extends StatelessWidget {
-  final String categoryId;
-  final String CategoryTitle;
+  // final String categoryId;
+  // final String CategoryTitle;
 
-  CategoryMealsScreens(this.categoryId, this.CategoryTitle);
+  // CategoryMealsScreens(this.categoryId, this.CategoryTitle);
 
   @override
   Widget build(BuildContext context) {
+    final routeArgs =
+        ModalRoute.of(context).settings.arguments as Map<String, String>;
+    final categoryId = routeArgs['id'];
+    final categoryTitle = routeArgs['title'];
     return Scaffold(
       appBar: AppBar(
-        title: Text(CategoryTitle),
+        title: Text(categoryTitle),
       ),
       body: Center(
         child: Text('The Recipes For The Coordinates!'),
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
-// import 'dart:ui';
-
 import 'package:flutter/material.dart';
+import 'package:flutter_complete_guide/category_meals_screen.dart';
 import './categories_screen.dart';
 
 void main() => runApp(MyApp());
@@ -31,6 +30,9 @@ class MyApp extends StatelessWidget {
             ),
       ),
       home: CategoriesScreen(),
+      routes: {
+        '/categories-meals': (cxt) => CategoryMealsScreens(),
+      },
     );
   }
 }
