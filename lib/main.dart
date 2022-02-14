7459a574b765093c63c26c93079eb7bc26040d00
OwethuSotomela <owethusotomela@gmail.com>
2022-02-14 09:36:17 +0200

    More navigation

--- lib/main.dart
+++ lib/main.dart
@@ -37,6 +37,13 @@ class MyApp extends StatelessWidget {
         CategoryMealsScreens.routeName: (cxt) => CategoryMealsScreens(),
         MealDetailScreen.routeName: (ctx) => MealDetailScreen(),
       },
+      onGenerateRoute: (settings){
+        print(settings.arguments);
+        return MaterialPageRoute(builder: (ctx)=> CategoriesScreen(),);
+      },
+      onUnknownRoute: (settings){
+        return MaterialPageRoute(builder: (ctx)=> CategoriesScreen(),);
+      } ,
     );
   }
 }
