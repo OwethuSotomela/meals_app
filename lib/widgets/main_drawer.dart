615ba57f8b7fbcbe32d51b294ab79a7dcb8938c3
OwethuSotomela <owethusotomela@gmail.com>
2022-02-17 20:12:27 +0200

    Adding a custom drawer

--- lib/widgets/main_drawer.dart
+++ lib/widgets/main_drawer.dart
@@ -1,6 +1,24 @@
 import 'package:flutter/material.dart';
 
 class MainDrawer extends StatelessWidget {
+  Widget buildListTile(String title, IconData icon) {
+    return ListTile(
+      leading: Icon(
+        icon,
+        size: 26,
+      ),
+      title: Text(
+        title,
+        style: TextStyle(
+          fontFamily: 'RobotoCondensed',
+          fontSize: 24,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+      onTap: () {},
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Drawer(
@@ -24,19 +42,8 @@ class MainDrawer extends StatelessWidget {
           SizedBox(
             height: 20,
           ),
-          ListTile(
-            leading: Icon(
-              Icons.restaurant,
-              size: 26,
-            ),
-            title: Text(
-              'Meals',
-              style: TextStyle(
-                fontFamily: 'RobotoCondensed',
-                fontSize: 24,
-              ),
-            ),
-          ),
+          buildListTile('Meals', Icons.restaurant),
+          buildListTile('Filters', Icons.settings),
         ],
       ),
     );
